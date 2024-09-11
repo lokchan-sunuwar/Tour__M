033fd034f8c08399362167c0fb70ec296a61ef24
onin <lokchanmuhkiya@gmail.com>
2024-09-11 18:22:02 +0545

    8

--- FrontEnd/src/components/Featured-tours/FeaturedTourList.jsx
+++ FrontEnd/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import TourCard from '../../Shared/TourCard'
-import tourData from '../../assets/data/tours'
 import {Col} from 'reactstrap'
+import useFetch from '../../hooks/useFetch'
+import {BASE_URL} from '../../utils/config'
 const FeaturedTourList = () => {
+  const {data:featuredTours,loading, error } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`)
+
+  console.log(featuredTours);
   return (
    <>
+{
+  loading && <h4>loading ....</h4>
+}
+{
+  error && <h4>{error}</h4>
+}
    {
-   tourData?.map(tour=>(
+    !loading && !error &&
+   featuredTours?.map(tour=>(
     <Col lg='3' className='mb-4'key={tour.id}><TourCard tour={tour}/></Col>
    )
    )}
